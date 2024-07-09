bbf56549a70e5671c42efdfc3ff82cb0bd3b4c66
Jissa  Joy <jissa.joy@kjr.com.au>
2024-07-09 17:13:49 +1000

    test updated

--- pages/SignupPage.js
+++ pages/SignupPage.js
@@ -7,7 +7,25 @@ class SignupPage {
         //this.email= page.getByPlaceholder('Email Address').filter({hasText: 'signup-email'})
         this.email = page.locator('//*[@id="form"]/div/div/div[3]/div/form/input[3]')
         this.signupBtn = page.getByRole('button', { name: 'Signup' })
+        //
+        this.title= page.getByLabel('Mrs.')
+        this.password = page.getByLabel('Password *')
+        this.day = page.locator('#days')
+        this.month= page.locator('#months')
+        //page.locator('#form form div').filter({ hasText: 'Year 2021 2020 2019 2018 2017' }).nth(2)
+           this.year = 
 
+          this.newsletterLabel = page.getByLabel('Sign up for our newsletter!')
+          this.specialOfferLable = page.getByLabel('Receive special offers from')
+          this.firstName = page.getByLabel('First name *')
+          this.lastName = page.getByLabel('Last name *')
+          this.address1 =page.getByLabel('Address * (Street address, P.')
+          this.address2= page.getByLabel('Address 2')
+          this.country = 
+          this.state = page.getByLabel('State *')
+          this.city= page.getByLabel('City *')
+          this.zip=page.locator('#zipcode')
+          this.mob = page.getByLabel('Mobile Number *')
 
     }
 
--- tests/test1.spec.js
+++ tests/test1.spec.js
@@ -1,37 +1,48 @@
 
-RECORDED SCRIPT
+/*RECORDED SCRIPT*/
 
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://www.automationexercise.com/');
   await page.getByRole('link', { name: ' Signup / Login' }).click();
-  await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').click();
-  await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').fill('testaccessibility2024@gmail.com');
-  await page.getByPlaceholder('Password').click();
-  await page.getByPlaceholder('Password').fill('test123');
-  await page.getByRole('button', { name: 'Login' }).click();
+  //page.locator('//*[@id="form"]/div/div/div[3]/div/form/input[3]')  //input[@placeholder='Name']
+  //await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').click();//*[@id="form"]/div/div/div[3]/div/form/input[2]
+  await page.getByPlaceholder('Name').fill('jj')
+  await page.locator('//*[@id="form"]/div/div/div[3]/div/form/input[3]').fill('testaccessibility2024@gmail.com')
+  await page.getByRole('button', { name: 'Signup' }).click()
+
   await expect(page.getByRole('link', { name: ' Home' })).toBeVisible();
-  await expect(page.getByText('Logged in as jisa')).toBeVisible();
-  await expect(page.getByRole('link', { name: ' Delete Account' })).toBeVisible();
+  //await page.getByRole('textbox', {name:password}).fill('test123')
+  await page.locator('//*[@id="password"]').fill('test123')
+ 
+ // await expect(page.getByText('Logged in as jisa')).toBeVisible();
+await page.locator('#days').selectOption({label:'2'})
+await page.locator('#months').selectOption({value:'8'})
+await page.locator('#years').selectOption({label:'1984'})
+await page.locator('#newsletter').check();
+await page.locator('#optin').check()
+await page.locator('#first_name').fill('joy')
+await page.locator('#last_name').fill('j')
+ await page.locator('#address1').fill('collins street')
+ await page.locator('#address2').fill('docklands')
+ await page.locator('#country').selectOption({label:"Australia"})
+ await page.locator('#state').fill('victoria')
+ await page.locator('#city').fill('melb')
+ await page.locator('#zipcode').fill('3000')
+ await page.locator('#mobile_number').fill('0400000000')
+ await page.getByRole('button',{name:'Create Account'}).click();
+ await expect(page.getByText('Account Created!')).toBeVisible();
+ 
+  await page.getByRole('link', { name: 'Continue' }).click();
+  //await page.frameLocator('iframe[name="aswift_1"]').getByLabel('Close ad').click();
+  await expect(page.getByText('Logged in as jj')).toBeVisible();
   await page.getByRole('link', { name: ' Delete Account' }).click();
-  await page.goto('https://www.automationexercise.com/');
-  await expect(page.getByRole('link', { name: ' Home' })).toBeVisible();
-  await page.getByRole('link', { name: ' Signup / Login' }).click();
-  await expect(page.getByRole('heading', { name: 'Login to your account' })).toBeVisible();
-  await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').click();
-  await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').fill('testaccessibility2024@gmail.com');
-  await page.getByPlaceholder('Password').click();
-  await page.getByPlaceholder('Password').fill('test1`121212');
-  await page.getByRole('button', { name: 'Login' }).click();
-  await expect(page.getByText('Your email or password is')).toBeVisible();
-  await page.getByPlaceholder('Password').click({
-    clickCount: 3
-  });
-  await page.getByPlaceholder('Password').fill('test123');
-  await page.getByRole('button', { name: 'Login' }).click();
-  await page.goto('https://www.automationexercise.com/delete_account');
-  await expect(page.locator('b')).toContainText('Account Deleted!');
-  https://www.automationexercise.com/
-  await expect(page.locator('b')).toContainText('Account Deleted!');
+  await expect(page.getByText('Account Deleted!')).toBeVisible();
+  await page.getByRole('link', { name: 'Continue' }).click();
+
+
+
+
+
 });
